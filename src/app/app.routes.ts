0f9d0899ec2c54103897bfdc3726bc1eacf150fc
Valentina Beca <126031586+valentina0612@users.noreply.github.com>
2025-09-24 23:03:42 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { AnalisisVideoComponent } from './analisis-video/analisis-video.component';
 
-
 export const routes: Routes = [
-  { path: '', component: AnalisisVideoComponent },
-  {path: '**', redirectTo: ''},  // Ruta comodín para manejar rutas no definidas
-  {path: 'analisis-video', component: AnalisisVideoComponent}
+  { path: '', component: AnalisisVideoComponent },  // Ruta raíz
+  { path: 'analisis-video', component: AnalisisVideoComponent },  // Ruta específica
+  { path: '**', redirectTo: '' }  // Ruta comodín AL FINAL
 ];
