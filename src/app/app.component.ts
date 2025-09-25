5d35a84d01b30908ff57158076db49bbe47059dd
Valentina Beca <126031586+valentina0612@users.noreply.github.com>
2025-09-24 22:51:56 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AnalisisVideoComponent } from './analisis-video/analisis-video.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AnalisisVideoComponent],
-  template: `<app-analisis-video></app-analisis-video>`
+  imports: [RouterOutlet],
+  template: `<router-outlet></router-outlet>`
 })
 export class AppComponent {
   title = 'frontAlertGuard';
