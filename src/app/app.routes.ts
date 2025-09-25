9fc5181672ad4075fdf489c613edd769e5b3d610
Valentina Beca <126031586+valentina0612@users.noreply.github.com>
2025-09-24 22:39:34 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AnalisisVideoComponent } from './analisis-video/analisis-video.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: AnalisisVideoComponent },
+];
