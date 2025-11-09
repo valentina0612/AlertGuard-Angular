4d7dcea2ab0a3cad2efa29bcaf7e9b8048fb83af
valentina0612 <vbecag@correo.usbcali.edu.co>
2025-11-08 20:41:26 -0500

    configuracion

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+app.use(express.static(path.join(__dirname, 'dist/front-alert-guard/browser')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/front-alert-guard/browser/index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
